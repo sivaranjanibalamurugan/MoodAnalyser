5aa7fa4641b9ca77a79443657eb7cce543bfce3b
unknown <sivaranjanib99@gmail.com>
2021-07-16 21:10:33 +0530

    [siva]UC4 creating the customexception

---
+++ MoodAnalyser/CustomAnalyseException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyser
+{
+    class CustomAnalyseException :Exception
+    {
+        //create instance of custom exception type
+        ExceptionType type;
+        //enum for defining constants
+        public enum ExceptionType
+        {
+            NULL_EXCEPTION,EMPTY_EXCEPTION
+        }
+        //parameterized constructor and also using base() method
+        public CustomAnalyseException(ExceptionType type, string message): base(message)
+        {
+            this.type = type;
+        }
+    }
+}
