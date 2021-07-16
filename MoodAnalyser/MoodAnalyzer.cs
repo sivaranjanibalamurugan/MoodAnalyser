21a7cc36066fd1d2ceeac3f1e537802dc7d0c535
unknown <sivaranjanib99@gmail.com>
2021-07-16 19:51:26 +0530

    [siva]UC1 creating a mood analyser

---
+++ MoodAnalyser/MoodAnalyzer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyze
+    {
+        //instance varriable
+        string message;
+       
+        public string AnalyseMood(string message)
+        {
+         
+            if (message.ToLower().Contains("happy"))
+            {
+                return "happy";
+            }
+            else
+            {
+                return "sad";
+            }
+        }
+    }
+}
---
+++ MoodAnalyserTest/UnitTest1.cs
@@ -0,0 +1,40 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyzer;
+namespace MoodAnalyzerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        MoodAnalyze moodAnalyzer;
+        string message = "I am in happy mood";
+        [TestInitialize]
+        public void Setup()
+        {
+            moodAnalyzer = new MoodAnalyze();
+        }
+      
+        [TestMethod]
+        public void ReturnHappyTest()
+        {
+            //assign
+            string expected = "happy";
+            
+            //act
+            string actual = moodAnalyzer.AnalyseMood(message);
+            //assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod]
+        public void ReturnSadTest()
+        {
+            //assign
+            string expected = "sad";
+            //act
+            string actual = moodAnalyzer.AnalyseMood("I am in Sad mood");
+            //assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
