5104110097d2cbe245d0f47c509b540087aebaa0
unknown <sivaranjanib99@gmail.com>
2021-07-20 13:16:10 +0530

    [siva]Constructor - refactoring moodanalyser with default constructor

--- MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserTest/UnitTest1.cs
@@ -5,22 +5,16 @@ namespace MoodAnalyzerTest
     [TestClass]
     public class UnitTest1
     {
-        MoodAnalyze moodAnalyzer;
-      
-        [TestInitialize]
-        public void Setup()
-        {
-            moodAnalyzer = new MoodAnalyze("message");
-        }
-
+       
         [TestMethod]
         public void ReturnHappyTest()
         {
             //assign
             string expected = "happy";
+            string message = "I am in Happy Mood";
 
             //act
-            string actual = moodAnalyzer.AnalyseMood("I am in happy mood");
+            string actual = new MoodAnalyze(message).AnalyseMood();
             //assert
             Assert.AreEqual(expected, actual);
 
@@ -30,8 +24,9 @@ namespace MoodAnalyzerTest
         {
             //assign
             string expected = "sad";
+            string message = "I am in Sad Mood";
             //act
-            string actual = moodAnalyzer.AnalyseMood("I am in Sad mood");
+            string actual = new MoodAnalyze(message).AnalyseMood();
             //assert
             Assert.AreEqual(expected, actual);
 
@@ -49,6 +44,83 @@ namespace MoodAnalyzerTest
             Assert.AreEqual(expected, actual);
 
         }
+        [TestMethod]
+        public void ReturnCustomException()
+        {
+            string expected = "Mood should not be empty";
+            try
+            {
+                string message = "";
+                //act
+                string actual = new MoodAnalyze(message).AnalyseMood();
+            }
+            catch (CustomAnalyseException ex)
+            {
+                //assert
+                Assert.AreEqual(expected, ex.Message);
+            }
+
+        }
+        [TestMethod]
+        public void ReturnMoodShoulNotBeNull()
+        {
+            string expected = "Mood should not be null";
+            try
+            {
+                string message = null;
+                //act
+                string actual = new MoodAnalyze(message).AnalyseMood();
+            }
+            catch (CustomAnalyseException ex)
+            {
+                //assert
+                Assert.AreEqual(expected, ex.Message);
+            }
+
+        }
+        /// <summary>
+        /// T.C=4.1 Returns the mood analyser object with reflection-> Equal
+        /// </summary>
+        [TestMethod]
+        public void ReturnMoodAnalyserObjectWithReflection()
+        {
+            object expected = new MoodAnalyze();
+            object actual = MoodAnalyseFactory.CreateObjectForMoodAnalyser("MoodAnalyzer.MoodAnalyze", "MoodAnalyze");
+            expected.Equals(actual);
+        }
+       
+        // Returns the mood analyser object with reflection1 -> class not found
+        
+        [TestMethod]
+        public void ReturnMoodAnalyserObjectWithReflection1()
+        {
+            string expected = "Class not found";
+            try
+            {
+                object actual = MoodAnalyseFactory.CreateObjectForMoodAnalyser("MoodAnalyzer.MoodAnalyzer", "MoodAnalyzer");
+            }
+            catch (CustomAnalyseException e)
+            {
+                Assert.AreEqual(expected, e.Message);
+            }
+        }
+       
+        //Returns the mood analyser object with reflection1 -> constructor not found
+        
+        [TestMethod]
+        public void ReturnMoodAnalyserObjectWithReflection2()
+        {
+            string expected = "Constructor not found";
+            try
+            {
+                object actual = MoodAnalyseFactory.CreateObjectForMoodAnalyser("MoodAnalyzer.MoodAnalyze", "MoodAnalyzer");
+            }
+            catch (CustomAnalyseException e)
+            {
+                Assert.AreEqual(expected, e.Message);
+            }
+        }
+
 
     }
 }
